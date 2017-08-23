4cf0c08f8833d32364d363058d9f1668dac70d9c
nut077 <nuttapon.tavee@gmail.com>
2017-08-23 14:03:16 +0700

    postcss

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,12 @@ module.exports = {
             }
           },
           {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
             loader: 'sass-loader',
             options: {
               sourceMap: true
