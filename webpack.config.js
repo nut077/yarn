589788f1492d956c6b38b83fcac7d5606b0cfd2f
Nut <nuttapon.tavee@gmail.com>
2017-08-23 15:49:46 +0700

    ESlint

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,8 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/, exclude: /node_modules/, loader: ['babel-loader']
-      },
+      { test: /\.js$/, exclude: /node_modules/, enforce: 'pre', loader: 'eslint-loader' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       {
         test: /\.scss$/,
         use: [
